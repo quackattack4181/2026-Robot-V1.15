934cccb7a2df8696989c27e69591f4abb295a7bf
QuackAttack-Team4181 <162037610+quackattack4181@users.noreply.github.com>
2026-01-29 17:05:30 -0600

    Add Limelight target distance dashboard readout

--- main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -36,6 +36,7 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -159,6 +160,8 @@ public class SwerveSubsystem extends SubsystemBase
       swerveDrive.updateOdometry();
       // vision.updatePoseEstimation(swerveDrive);
     }
+    SmartDashboard.putNumber("Limelight Target Distance (ft)",
+                             getLimelightTargetDistanceFeet(Constants.VisionConstants.LIMELIGHT_NAME));
   }
 
   @Override
@@ -664,6 +667,22 @@ public class SwerveSubsystem extends SubsystemBase
     }
     Pose3d targetPose = LimelightHelpers.getTargetPose3d_RobotSpace(limelightName);
     return Math.hypot(targetPose.getX(), targetPose.getY());
+  }
+
+  /**
+   * Get the planar distance to the current Limelight target in feet.
+   *
+   * @param limelightName Limelight network table name.
+   * @return Distance to target in feet, or {@code Double.NaN} if no target.
+   */
+  public double getLimelightTargetDistanceFeet(String limelightName)
+  {
+    double distanceMeters = getLimelightTargetDistanceMeters(limelightName);
+    if (Double.isNaN(distanceMeters))
+    {
+      return Double.NaN;
+    }
+    return Units.metersToFeet(distanceMeters);
   }
 
   /**
